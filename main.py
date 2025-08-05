558e2860349e5eaff55b934f50b640b1517ee935
Kartik <bansodekartik00@gmail.com>
2025-08-05 22:53:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from cohere_bot import generate_post
-from chirr_bot import post_to_twitter_via_chirr
+from chirr_bot import post_to_chirr
 
-
-print("Generating post with Cohere...")
 tweet = generate_post()
-
-if tweet:
-    print("✅ Post generated successfully.")
-    print("🔁 Launching headless browser...")
-    post_to_twitter_via_chirr(tweet.strip())
-
-else:
-    print("❌ No tweet was generated.")
+post_to_chirr(tweet)
